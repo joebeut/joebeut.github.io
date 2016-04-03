ec393f4b46ad4e2cdd0294479953d47013fa2d06
Joe Beutler <jtb2133@columbia.edu>
2016-04-03 00:18:48 -0400

    initial for5 commit

---
+++ about.php
@@ -0,0 +1,27 @@
+
+<?php
+  /*
+    about.php
+  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include './parts/header.php'; ?>
+</head>
+
+<body>
+<div id="container">
+  <?php include './parts/navbar.php'; ?>
+
+  <div class="container-fluid" id="body">
+    <?php include './body/about.php'; ?>
+  </div>
+
+  <?php include './parts/footer.php'; ?>
+  
+</div>
+</body>
+</html>
---
+++ parts/footer.php
@@ -0,0 +1,6 @@
+
+<div class="container-fluid" id="footer">
+	<div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	  <p class="for5-green">&copy2016 for5</p>
+	</div>
+</div>
