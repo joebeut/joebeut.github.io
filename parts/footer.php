4337dddbe519d3a5bb579e7bd9c605773ab3316e
Joe Beutler <joe@eatfreqs.com>
2017-09-28 19:43:42 -0500

    First pages commit

--- parts/footer.php
+++
@@ -1,6 +0,0 @@
-
-<div class="container-fluid" id="footer">
-	<div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	  <p class="for5-green">&copy2016 for5</p>
-	</div>
-</div>
--- parts/header.php
+++
@@ -1,6 +0,0 @@
-
-<title>for5</title>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="./stylesheets/style.css" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="favicon.ico" type="image/x-icon" />
